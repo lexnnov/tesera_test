1f7f2fa56b85484ddeec41f8aff035e8bd542ccf
Alexandr <lexnnov2009@gmail.com>
2021-03-19 18:13:07 +0300

    first commit

---
+++ pages/game/_id.vue
@@ -0,0 +1,90 @@
+<template>
+  <el-container class="game-information">
+    <link-button class="game-information_back-button" link="/" button-text="К списку игр"/>
+    <el-main v-if="gameInformation">
+      <el-row class="game-information_main">
+        <el-col class="game-information_left" :span="6">
+          <div class="grid-content bg-purple">
+            <el-image class="game-card_image" :src="gameInformation.photoUrl"/>
+          </div>
+        </el-col>
+        <el-col class="game-information_right" v-if="gameInformation" :span="18">
+          <div class="game-information_title">{{ gameInformation.title }}</div>
+          <div class="game-information_description" v-html="gameInformation.description"/>
+        </el-col>
+      </el-row>
+      <comment-block :comments="comments" :game-id="gameInformation.teseraId"/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  import LinkButton from '../components/LinkButton/LinkButton'
+  import CommentBlock from '../components/CommentBlock/CommentBlock'
+
+  export default {
+    components: {
+      LinkButton,
+      CommentBlock
+    },
+    data () {
+      return {
+        dataLoaded: false
+      }
+    },
+    computed: {
+      comments () {
+        if (this.dataLded) {
+          return this.$store.state.comments.comments[this.$store.state.comments.gameInfo.game.teseraId].comments
+        } else {
+          return []
+        }
+      },
+      gameInformation () {
+        return this.$store.state.comments.gameInfo.game
+      },
+      dataLded () {
+        return this.$store.state.comments.fullyLoaded
+      }
+    },
+    created () {
+      this.$store.dispatch('default.js/getGame', { self: this.$route.params.id })
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+  }
+
+  .comments {
+    margin-top: 20px;
+    padding-top: 20px;
+    border-top: 1px solid #d2d2d2;
+  }
+
+  .game-information {
+    &_back-button {
+      margin: 0 auto;
+    }
+
+    &_main {
+      display: flex;
+      align-items: center;
+    }
+
+    &_right {
+      padding-left: 25px;
+    }
+
+    &_title {
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+
+</style>
